43984be79131eb4ccd57a1db74e5cd6e589f3c57
Utente <Utente@PCFRATAGW7>
2016-06-09 14:55:20 +0200

    ee

--- TestChess/src/org/berserk73/core/TestChess.java
+++ TestChess/src/org/berserk73/core/TestChess.java
@@ -4,12 +4,9 @@ public class TestChess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//test merge
-//test2
-		//test2
+
 System.out.println("sdfsdf");
 System.out.println("dfsdfdsf");
-//test2
 	}
 
 }
